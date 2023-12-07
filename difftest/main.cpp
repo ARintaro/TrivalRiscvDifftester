0a59b2db475f052076b410492a0dd94a2f382f00
r1ntaro <rintaro@foxmail.com>
2023-12-07 23:14:01 +0800

    server mode

--- difftest/main.cpp
+++ difftest/main.cpp
@@ -3,6 +3,9 @@
 #include "elf_reader.h"
 #include "test_core.h"
 #include "virtual_device.h"
+#include <asm-generic/socket.h>
+#include <csignal>
+#include <cstddef>
 #include <cstdio>
 #include <iostream>
 #include <netinet/in.h>
@@ -12,9 +15,16 @@
 
 #include "disasm.h"
 
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
+const unsigned max_step = 100000;
+
+const u32 ebreak = 0x100073;
+
+bool received_term = false;
+int ret_value = 0;
 
 std::string get_path_extension(const std::string &path) {
   auto pos = path.find_last_of('.');
@@ -24,8 +34,11 @@ std::string get_path_extension(const std::string &path) {
   return "";
 }
 
-
-
+void signal_handler(int signum) {
+  puts("Receive Signal, exit...");
+  received_term = true;
+  ret_value = signum;
+}
 
 int main(int argc, char **argv) {
   TestProgramConfig config{
@@ -35,13 +48,16 @@ int main(int argc, char **argv) {
       .memory_size = 1024 * 1024 * 8,
       .uart_config = VirtualDeviceConfig{.start = 0x10000000, .size = 8}};
 
-  if (argc != 5) {
+
+  
+  if (argc != 6) {
     std::cerr << "Usage: " << argv[0]
-              << " <spike_file> <tested_file> <Print> <data_file>" << std::endl;
+              << " <spike_file> <tested_file> <print> <data_file> <uart_server>" << std::endl;
     return 1;
   }
 
   auto print = std::string(argv[3]) == "true";
+  bool uart_server = std::string(argv[5]) == "true";
 
   std::cout << "Print: " << print << std::endl;
 
@@ -67,121 +83,147 @@ int main(int argc, char **argv) {
 
   unsigned instr_step = 0;
 
-  const unsigned max_step = 100000;
-
-  const u32 ebreak = 0x100073;
-
-  std::queue<u8> uart_buffer;
-
-  int listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (listenfd == -1) {
-    std::cerr << "socket error" << std::endl;
-    return 1;
-  } 
-
   int bind_port = 8080;
 
+  std::queue<u8> uart_buffer;
+  int listenfd = 0;
   sockaddr_in addr;
   addr.sin_family = AF_INET;
   addr.sin_port = htons(bind_port);
   addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-  if (bind(listenfd, (sockaddr *)&addr, sizeof(addr)) == -1) {
-    std::cout << "bind error" << std::endl;
-    return 1;
-  }
+  if (uart_server) {
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    // setsockopt(listenfd, SOL_SOCKET , SO_REUSEADDR, &opt, sizeof(opt));
+    if (listenfd == -1) {
+      std::cerr << "socket error" << std::endl;
+      return 1;
+    }
+    if (bind(listenfd, (sockaddr *)&addr, sizeof(addr)) == -1) {
+      std::cout << "bind error on port " << bind_port << std::endl;
+      return 1;
+    }
 
-  if (listen(listenfd, 1) == -1) {
-    std::cout << "listen error" << std::endl;
-    return 1;
+    if (listen(listenfd, 1) == -1) {
+      std::cout << "listen error" << std::endl;
+      return 1;
+    }
   }
 
   sockaddr_in client_addr;
   socklen_t client_addr_len = sizeof(client_addr);
-
   int clientfd = 0;
-  std::clog << "waiting connect, watching " << bind_port << std::endl;
-  
-  do {
-    clientfd = accept(listenfd, (sockaddr *)&client_addr, &client_addr_len);
-    
-  } while(clientfd == -1);
 
-  std::clog << "client connected" << std::endl;
+
+  if (uart_server) {
+    std::clog << "waiting connect, watching " << bind_port << std::endl;
+
+    clientfd = accept(listenfd, (sockaddr *)&client_addr, &client_addr_len);
+
+    if (received_term) {
+      std::clog << "client not connected" << std::endl;
+      goto EXIT;
+    }
+
+    std::clog << "client connected" << std::endl;
+  }
+
+  std::signal(SIGINT, signal_handler);
+  std::signal(SIGTERM, signal_handler);
 
   u8 recvBuffer[1024];
 
 
-  while(true) {
-    int recvLen = recv(clientfd, recvBuffer, sizeof(recvBuffer), MSG_DONTWAIT);
-    if (recvLen > 0) {
-      for (int j = 0; j < recvLen; j++) {
-        uart_buffer.push(recvBuffer[j] & 0xff);
-        printf("[difftest] send 0x%x\n", recvBuffer[j] & 0xff);
+  try {
+    while (!received_term && (uart_server || instr_step < max_step)) {
+
+      if (uart_server) {
+        int recvLen =
+            recv(clientfd, recvBuffer, sizeof(recvBuffer), MSG_DONTWAIT);
+        if (recvLen > 0) {
+          for (int j = 0; j < recvLen; j++) {
+            uart_buffer.push(recvBuffer[j] & 0xff);
+            printf("[difftest] send 0x%x\n", recvBuffer[j] & 0xff);
+          }
+        }
+
+        if (uart_buffer.size()) {
+          if (tested.devices.uart->host_write_byte(uart_buffer.front())) {
+            uart_buffer.pop();
+          }
+        }
       }
-    }
 
-    if (uart_buffer.size()) {
-      if (tested.devices.uart->host_write_byte(uart_buffer.front())) {
-        uart_buffer.pop();
-      }
-    }
-
-    auto result = tested.core->step();
-
-    if (print) {
-      std::cerr<< "step_num : " << result.step_num << std::endl;
-    }
-
-    auto& test_state = tested.core->get_state();
-
-    if (result.uncertern) {
-      // 非确定转移，同步状态
-      golden.core->set_state(test_state);
-      // std::cerr<< "uncertern sync state" << result.step_num << std::endl;
-      // test_state.print();
-      continue;
-    } 
-
-    for (int j = 0; j < result.step_num; j++) {
-      golden.core->step();
-
-      auto last_inst = golden.core->get_state().last_inst;
+      auto result = tested.core->step();
 
       if (print) {
-        std::cerr << disasm->disassemble(last_inst) << std::endl;
+        std::cerr << "step_num : " << result.step_num << std::endl;
       }
 
-      if (last_inst == ebreak) {
-        return 0;
+      auto &test_state = tested.core->get_state();
+
+      if (result.uncertern) {
+        // 非确定转移，同步状态
+        golden.core->set_state(test_state);
+        // std::cerr<< "uncertern sync state" << result.step_num << std::endl;
+        // test_state.print();
+        continue;
+      }
+
+      for (int j = 0; j < result.step_num; j++) {
+        golden.core->step();
+
+        auto last_inst = golden.core->get_state().last_inst;
+
+        if (print) {
+          std::cerr << disasm->disassemble(last_inst) << std::endl;
+        }
+
+        if (last_inst == ebreak) {
+          ret_value = 0;
+          received_term = true;
+        }
+      }
+
+      auto uart_out = tested.devices.uart->host_read_byte();
+      if (uart_out.has_value()) {
+        send(clientfd, &uart_out.value(), 1, 0);
+      }
+
+      instr_step += result.step_num;
+
+      const auto &state = golden.core->get_state();
+      const auto &state_tested = tested.core->get_state();
+
+      if (state != state_tested && !received_term) {
+        std::cerr << "not matched" << std::endl;
+        std::cerr << "== golden state ==" << std::endl;
+        state.print();
+        std::cerr << "== test core state ==" << std::endl;
+        state_tested.print();
+
+        ret_value = 1;
+        received_term = true;
+      }
+
+      if (print) {
+        test_state.print();
       }
     }
 
-    auto uart_out = tested.devices.uart->host_read_byte();
-    if (uart_out.has_value()) {
-      send(clientfd, &uart_out.value(), 1, 0);
-    }
-
-
-    instr_step += result.step_num;
-
-    const auto &state = golden.core->get_state();
-    const auto &state_tested = tested.core->get_state();
-
-    if (state != state_tested) {
-      std::cerr << "not matched" << std::endl;
-      state.print();
-      state_tested.print();
-      return 1;
-    }
-
-    if (print) {
-      test_state.print();
-    }
+  } catch (...) {
     
   }
-  puts("");
-  std::cout << "Difftest Over " << std::endl;
+
+EXIT:
+
+  puts("==============");
+  std::cout << "Difftest Over with " << ret_value << " " << std::endl;
   std::cout << "total steps: " << instr_step << std::endl;
-  return 0;
+
+  close(clientfd);
+  close(listenfd);
+
+  return ret_value;
 }
